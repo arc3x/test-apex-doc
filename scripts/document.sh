ada7315d3895b86f8e5e1a4045151b4c003ed1d6
Matthew Slocum <matthew@redteal.com>
2017-04-25 14:49:28 -0700

    travis config

--- scripts/document.sh
+++ scripts/document.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pwd && java -jar apexdoc.jar -s 'src/classes' -t 'documentation' -p 'global;public;private;testmethod;webService' -g 'https://github.com/arc3x/test-apex-doc/tree/master/src/classes'   
+pwd && java -jar apexdoc.jar -s 'src/classes' -t 'documentation' -p 'global;public;private;testmethod;webService' -g 'https://github.com/arc3x/test-apex-doc/tree/master/src/classes/'   
