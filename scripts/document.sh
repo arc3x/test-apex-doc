49635895ed904d92890b5e66fc2a00e4b109f7a7
Matthew Slocum <matthew@redteal.com>
2017-04-26 12:34:26 -0700

    travis config

--- scripts/document.sh
+++ scripts/document.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 java -jar apexdoc.jar -s 'src/classes' -t 'documentation' -p 'global;public;private;testmethod;webService' -g "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}/tree/master/src/classes/"
+
 cd documentation/ApexDocumentation
 echo "ls"
 ls
 cp * ../../*
 echo "ls"
 ls
-cd ../..
+cd ../../
 echo "ls"
