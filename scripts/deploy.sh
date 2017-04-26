11358ec7b215be464a0aeb2ab0a5f235be483cb7
Matthew Slocum <matthew@redteal.com>
2017-04-26 11:59:04 -0700

    travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,25 +2,23 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
 rev=$(git rev-parse --short HEAD)
 
 cd documentation/ApexDocumentation
 
-git init
-git config user.name "RedTeal - Autodoc"
-git config user.email "admin@redteal.com"
-
-git remote add upstream "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-git fetch upstream
-git reset upstream/gh-pages
+# git init
+# git config user.name "RedTeal - Autodoc"
+# git config user.email "admin@redteal.com"
+#
+# git remote add upstream "https://${GH_TOKEN}@github.com/test-apex-doc.git"
+# git fetch upstream
+# git reset upstream/gh-pages
 
 # echo "test.com" > CNAME
+
+git fetch origin/gh-pages
+git reset origin/gh-pages
+git checkout gh-pages
 
 touch .
 
